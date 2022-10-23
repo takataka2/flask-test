da9a960e74289ed7e653449bc34bdaa331434112
takataka2 <mat.tak.96039@gmail.com>
2022-10-23 10:37:50 +0900

    templates追加

--- index.py
+++ index.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-   return "<p>Hello, World!</p>"
+   return "<p>Root!</p>"
+
+@app.route('/index')
+def index():
+    return render_template('index.html') # templatesフォルダ内のindex.htmlを表示する
+
